a428dd435cf6433ba811c670e5e6a7bf115e333a
CJQuintero61 <cjquintero61@gmail.com>
2025-11-21 10:00:10 -0600

    initial commit

---
+++ banker.c
@@ -0,0 +1,24 @@
+/*
+    banker.c
+    For COSC 3346 Operating Systems proj4
+    Created 11/21/2025 by Christian Quintero
+    Last updated 11/21/2025 by Christian Quintero
+
+    This program implements the banker's algorithm for
+    resource allocation.
+
+    To run:
+    1. 
+    2.
+    3.
+    4.
+*/
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+
+
+
+    return 0;
+}
