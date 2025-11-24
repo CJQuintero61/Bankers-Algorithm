ed6a20b41877051719728bcd9c7b350a1ec1b4a4
CJQuintero61 <cjquintero61@gmail.com>
2025-11-24 17:51:14 -0600

    first fully complete version

--- banker.c
+++ banker.c
@@ -2,7 +2,7 @@
     banker.c
     For COSC 3346 Operating Systems proj4
     Created 11/21/2025 by Christian Quintero
-    Last updated 11/21/2025 by Christian Quintero
+    Last updated 11/24/2025 by Christian Quintero
 
     This program implements the banker's algorithm for
     resource allocation.
@@ -19,9 +19,11 @@ void print_alloc_matrix(int num_processes, int num_resource_types, int** alloc);
 void print_max_matrix(int num_processes, int num_resource_types, int** max);
 void print_need_matrix(int num_processes, int num_resource_types, int** need);
 void print_available_vector(int num_resource_types, int* available);
+void print_request_vector(int num_resource_types, int* request, int process_request);
 void free_matrix(int num_resource_types, int** matrix);
 int** create_matrix(int num_processes, int num_resource_types);
 void check_state(int num_processes, int num_resource_types, int** alloc, int** need, int* available);
+int check_request(int num_processes, int num_resource_types, int process_request, int* request, int** alloc, int* available);
 
 int main(int argc, char *argv[]) {
     /*
@@ -39,6 +41,8 @@ int main(int argc, char *argv[]) {
     FILE* file_pointer = NULL;
     int num_processes = 0;
     int num_resource_types = 0;
+    int process_request = 0;
+    int can_grant_request = 0;
 
     // open the file in read only mode
     file_pointer = fopen(argv[1], "r");
@@ -56,6 +60,7 @@ int main(int argc, char *argv[]) {
     int** max = create_matrix(num_processes, num_resource_types);
     int** need = create_matrix(num_processes, num_resource_types);
     int* available = malloc(num_resource_types * sizeof(int));
+    int* request = malloc(num_resource_types * sizeof(int));
 
     // read the allocation matrix 
     for(int i = 0; i < num_processes; i++) {
@@ -83,6 +88,7 @@ int main(int argc, char *argv[]) {
         fscanf(file_pointer, "%d", &available[i]);
     }
 
+    // print stats before doing requests
     printf("Number of Processes: %d\nNumber of Resources: %d\n", num_processes, num_resource_types);
     print_alloc_matrix(num_processes, num_resource_types, alloc);
     print_max_matrix(num_processes, num_resource_types, max);
@@ -92,11 +98,33 @@ int main(int argc, char *argv[]) {
     // check state
     check_state(num_processes, num_resource_types, alloc, need, available);
 
+    // read the requests
+    while(fscanf(file_pointer, "%d:", &process_request) == 1) {
+
+        // read the rest of the request
+        for(int i = 0; i < num_resource_types; i++) {
+            fscanf(file_pointer, "%d", &request[i]);
+        }
+
+        // print the request
+        print_request_vector(num_resource_types, request, process_request);
+
+        // check if the request can be granted
+        can_grant_request = check_request(num_processes, num_resource_types, process_request, request, alloc, available);
+
+        // update the available vector if we could grant it
+        if(can_grant_request) {
+            print_available_vector(num_resource_types, available);
+        }
+
+    }
+
     // free the matricies and vector
     free_matrix(num_processes, alloc);                  // alloc matrix
     free_matrix(num_processes, max);                    // max matrix
     free_matrix(num_processes, need);                   // need matrix
     free(available);                                    // available vector
+    free(request);                                      // request vector
 
     // close the file and return 0
     fclose(file_pointer);
@@ -231,7 +259,7 @@ int** create_matrix(int num_processes, int num_resource_types) {
 
 void check_state(int num_processes, int num_resource_types, int** alloc, int** need, int* available) {
     /*
-        commutes if the system is in a safe state
+        computes if the system is in a safe state
     */
 
     int work[num_resource_types];
@@ -299,9 +327,69 @@ void check_state(int num_processes, int num_resource_types, int** alloc, int** n
 
     
     if(safe) {
-        printf("The system is in a safe state.\n");
+        printf("The system is in a safe state.\n\n");
     }
     else {
-        printf("The system is NOT in a safe state.\n");
+        printf("The system is NOT in a safe state.\n\n");
     }
+}
+
+
+int check_request(int num_processes, int num_resource_types, int process_request, int* request, int** alloc, int* available) {
+    /*
+        checks if a request can be granted 
+
+        params:
+            num_processes: int - the number of different processes. This is the ROW count in the matricies
+            num_resource_type: int - the number of different resource types. This is the COL count in the matricies
+            process_request: int - the process number making the request.
+            request: int* - the requested amounts for each resource type for process <process_request number> 
+            alloc: int** - the allocation array for what each process already has
+            available: int* - the available resources currently
+
+        returns:
+            int - 1 if the request can be granted. 0 otherwise.
+    */
+
+    // iterate through each resource request
+    for(int i = 0; i < num_resource_types; i++) {
+
+        // for each resource type, check if the sum of what has already been
+        // alocated + the request for resource i is > what is available for resource i, 
+        // then we cannot grant the request.
+        if(alloc[process_request][i] + request[i] > available[i]) {
+            printf("The request for process [%d] cannot be granted.\n\n", process_request);
+            return 0;
+        }
+    }
+
+    // if all the resources can be granted for each resource type, then we can grant
+    printf("The request for process [%d] can be granted!\n\n", process_request);
+
+    // update the available vector
+    for(int i = 0; i < num_resource_types; i++) {
+        available[i] = available[i] - request[i];
+    }
+
+    return 1;
+}
+
+
+void print_request_vector(int num_resource_types, int* request, int process_request) {
+    /*
+        prints the request vector
+    */
+    printf("Request Vector for process [%d]:\n", process_request);
+    printf("   ");
+
+    // dynamically print the resource types
+    for(int i = 0; i < num_resource_types; i++) {
+        printf("%c ", 'A' + i);
+    }
+    printf("\n   ");
+
+    for(int i = 0; i < num_resource_types; i++) {
+        printf("%d ", request[i]);
+    }
+    printf("\n\n");
 }
